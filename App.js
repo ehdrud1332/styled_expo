38e0a4f5be7657777b66d33a90196eaa744ec00c
CastleJaeMin <ehdrud1332@gmail.com>
2020-10-31 14:55:11 +0900

    Stack Update #2

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-export default function App() {
+import IntrodusScreen from './screen/IntrodusScreen';
+import MainScreen from './screen/MainSreen';
+import DetailScreen from './screen/DetailScreen'
+
+const MainStack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainStack.Navigator headerMode='none'>
+          <MainStack.Screen
+              name='IntrodusScreen'
+              component={IntrodusScreen}
+          />
+          <MainStack.Screen
+              name='MainScreen'
+              component={MainScreen}
+          />
+          <MainStack.Screen
+              name='DetailScreen'
+              component={DetailScreen}
+          />
+      </MainStack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screen/IntrodusScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const IntrodusScreen = () => {
+    return (
+        <View>
+            <Text>IntrodusScreen</Text>
+        </View>
+    );
+};
+
+export default IntrodusScreen;
